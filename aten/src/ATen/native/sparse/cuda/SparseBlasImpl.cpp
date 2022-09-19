42abedc550717626059cf6939388cfd9655f6e23
Andrew M. James <andrew.m.james2@gmail.com>
2022-09-19 17:39:58 -0500

    Enable dense x bsc mm/addmm
    ghstack-source-id: f40a1e153d5b248836a913bcb0b47ab0689ced8f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85308

--- aten/src/ATen/native/sparse/cuda/SparseBlasImpl.cpp
+++ aten/src/ATen/native/sparse/cuda/SparseBlasImpl.cpp
@@ -856,6 +856,15 @@ void addmm_out_sparse_csr(
             alpha,
             result.transpose(-2, -1));
       }
+    } else if (mat2.layout() == kSparseBsc) {
+      if (result.layout() == kStrided) {
+        return block_sparse_mm(
+            mat2.transpose(-2, -1),
+            mat1.transpose(-2, -1),
+            beta,
+            alpha,
+            result.transpose(-2, -1));
+      }
     }
   } else if (mat1.layout() == kSparseCsr) {
     if (mat2.layout() == kStrided) {
